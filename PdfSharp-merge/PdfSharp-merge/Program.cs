c65671b93078d204bd0ec860e5257d23fe4fd47a
Francesco-Vitolo <94391020+Francesco-Vitolo@users.noreply.github.com>
2022-06-09 16:13:45 +0200

    Add files via upload

---
+++ PdfSharp-merge/PdfSharp-merge/Program.cs
@@ -0,0 +1,35 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.IO;
+
+namespace PdfSharp_merge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\test";
+            string newFile = $@"{path}\merged.pdf";
+            string[] PdfFiles = Directory.GetFiles(path, "*.pdf", SearchOption.TopDirectoryOnly);
+            PdfDocument document = new PdfDocument();
+
+            foreach (string pdfFile in PdfFiles)
+            {
+                PdfDocument inputPDFDocument = PdfReader.Open(pdfFile, PdfDocumentOpenMode.Import);
+                document.Version = inputPDFDocument.Version;
+                foreach (PdfPage page in inputPDFDocument.Pages)
+                {
+                    document.AddPage(page);
+                }
+            }
+            if (File.Exists(newFile))
+            {
+                File.Delete(newFile);
+            }            
+            document.Save(newFile);
+            Console.WriteLine($"{newFile} wurde erstellt");
+            Console.ReadKey();
+        }
+    }
+}
